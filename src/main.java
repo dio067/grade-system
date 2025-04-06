44344b0d9bfddb0017532539027901efa756e35a
dio067 <yousefna06@gmail.com>
2025-04-07 00:10:23 +0400

    feat: declaring essential methods without logic

--- src/main.java
+++ src/main.java
@@ -7,18 +7,37 @@ class Main {
 class Student {
     private String studentName;
     private int studentId;
+    private String[] listStudent;
+
     //Getters
     public String getName(){ return "nothing";};
     public String getId(){ return "nothing";};
+    public String[] getListStudent(){ return listStudent;}
+
     //Setters
     public void setId(){};
     public void setName(){};
+    public void setListStudent(){};
+
+    //methods
+    public void addStudent(){};
 
 }
 class Course {
     private String courseName;
+    private String[] listCourses;
+    
     //Getters
     public String getCourseName(){return "nothing";};
+    public String[] getListCourses(){ return listCourses;};
+    
+    //Setters
     public void setCourseName(){};
+    public void setListCourses(){};
+
+    //methods
+    public void addCourse(){};
+    public void dropCourse(){};
+
     
 }
