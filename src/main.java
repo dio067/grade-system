e72649bfffd3bfe5d1414886eac546e6ce307f3b
dio067 <yousefna06@gmail.com>
2025-04-06 23:46:23 +0400

    initial commit

---
+++ src/main.java
@@ -0,0 +1,24 @@
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Still Nothing");
+    }
+}
+class Student {
+    private String studentName;
+    private int studentId;
+    //Getters
+    public String getName(){ return "nothing";};
+    public String getId(){ return "nothing";};
+    //Setters
+    public void setId(){};
+    public void setName(){};
+
+}
+class Course {
+    private String courseName;
+    //Getters
+    public String getCourseName(){return "nothing";};
+    public void setCourseName(){};
+    
+}
