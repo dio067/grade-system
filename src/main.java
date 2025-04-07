206ffd30864c6263b6d6b8acb26958e1d48a0841
dio067 <yousefnas06@gmail.com>
2025-04-07 19:05:39 +0400

    feat: add viewTranscript,assignGrade methods to System class

--- src/main.java
+++ src/main.java
@@ -11,25 +11,26 @@ class Student {
     private int studentId;
     private static ArrayList<Course> registeredCourses = new ArrayList<>();
 
+
     // Getters
     public String getName() { return studentName; }
     public int getId() { return studentId; }
     public ArrayList<Course> getRegisteredCourses() { return registeredCourses; }
-
     // Setters
     public void setId(int studentId) { this.studentId = studentId; }
     public void setName(String studentName) { this.studentName = studentName; }
 
     // Methods
     public void studentInformation() {
-        System.out.println("Student Name: " + studentName + "\nStudent Id: " + studentId);
+        System.out.println("Student Name: " + getName() + "\nStudent Id: " + getId()  + "\nRegisterd Courses: " + getRegisteredCourses() + "\nGrades: " + getCourseGrades());
     }
 }
 
 class Course {
-    private String courseName;
+    private String courseTitle;
     private int creditHour, courseId;
     private boolean isAvailable;
+    private double grade;
 
     // Constructor
     public Course() {
@@ -37,16 +38,21 @@ class Course {
     }
 
     // Getters
-    public String getCourseName() { return courseName; }
+    public String getCourseTitle() { return courseTitle
+; }
     public int getCreditHour() { return creditHour; }
     public int getCourseId() { return courseId; }
     public boolean getAvailability() { return isAvailable; }
+    public double getGrade() { return grade; }
 
     // Setters
-    public void setCourseName(String courseName) { this.courseName = courseName; }
+    public void setcourseTitle(String courseTitle) { this.courseTitle
+ = courseTitle
+; }
     public void setCreditHour(int creditHour) { this.creditHour = creditHour; }
     public void setCourseId(int courseId) { this.courseId = courseId; }
     public void setAvailability(boolean condition) { isAvailable = condition; }
+    public void setGrade(double grade) { this.grade = grade; }
 }
 
 class System {
@@ -79,7 +85,8 @@ class System {
     public void displayCourses() {
         for (Course course : listCourses) {
             System.out.println("Course ID: " + course.getCourseId());
-            System.out.println("Course Name: " + course.getCourseName());
+            System.out.println("Course Name: " + course.getCourseTitle
+    ());
         }
     }
 
@@ -111,7 +118,7 @@ class System {
     }
 
     public void dropCourse(Student student, int courseId) {
-        for (Course course : listCourses) {
+        for (Course course : student.getRegisteredCourses()) {
             if (course.getCourseId() == courseId && !course.getAvailability()) {
                 student.getRegisteredCourses().remove(course);
                 course.setAvailability(true);
@@ -119,5 +126,21 @@ class System {
                 return;
             }
         }
+    }
+    public void assignGrade(Student student, int courseId, double grade ){
+        for(Course course : student.getRegisteredCourses()) {
+            if(course.getCourseId() == courseId && course.getAvailability()){
+                course.setGrade(grade);
+                return;
+            }
+        
+        }
+        System.out.println("CANNOT ASSIGN GRADE, COURSE DOESN'T MATCH ANYONE ON YOUR SCHEDULE!!");
+    }
+    public void viewTranscript(Student student){
+          for(Course course : student.getRegisteredCourses()){
+            System.out.println("Course Name: " + course.getCourseTitle
+    () + "Course Grade: " + course.getGrade());
+          }
     }
 }
